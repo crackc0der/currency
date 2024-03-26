db4ad75b8a8d7a2aa19495e6025a1c208493c102
crackc0der <natsaev1989@gmail.com>
2024-03-26 13:53:24 +0300

    added service's mthods

--- internal/currency/service.go
+++ internal/currency/service.go
@@ -19,11 +19,32 @@ func (s Service) GetCurrencies(ctx context.Context) ([]Currency, error) {
 	return currencies, nil
 }
 
-func (s Service) GetCurrency(ctx context.Context, currency string) (Currency, error) {
+func (s Service) GetCurrency(ctx context.Context, currencyName string) (*Currency, error) {
+	currency, err := s.repository.SelectCurrency(ctx, currencyName)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get currency in method GetCurrency: %w", err)
+	}
+
+	return currency, nil
 }
 
 func (s Service) SetCurrencies(ctx context.Context, currencies []Currency) ([]Currency, error) {
+	currencies, err := s.repository.InsertCurrencies(ctx, currencies)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert currencies in method SetCurrencies: %w", err)
+	}
+
+	return currencies, nil
 }
 
-func (s Service) GetChangesPerHour(ctx context.Context, currency string) (Currency, error) {
+func (s Service) GetChangesPerHour(ctx context.Context, currency string) (float64, error) {
+	change, err := s.repository.SelectChangesPerHour(ctx, currency)
+
+	if err != nil {
+		return -1, fmt.Errorf("error getting changes per hour in method GetChangesPerHour: %w", err)
+	}
+
+	return change, nil
 }
